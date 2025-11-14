cb50e1837586bf94df7693c766adc3ccb3e3022b
kamui-akatuki <kamui-akatuki@kamui-akatukinoMacBook-Air.local>
2025-11-14 20:01:22 +0900

    クソデカ仕様変更に対応した変更

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Diff, Event, FileRequest, GameData, ModBase, MoveEvent, Player, RequestExpansion, RequestUpdater, SquareRequest, StartEvent } from "shogi2-types";
+import { Event, FileRequest, GameData, ModBase, MoveEvent, Player, Request, RequestUpdater, SquareRequest, StartEvent } from "shogi2-types";
 import fs from "fs";
 
 export default class Ennui extends ModBase{
   constructor(){
     super();
   }
-  onStart(_data:GameData,_before:GameData,_event:StartEvent,_sender:Player,_updater:RequestUpdater):{r:RequestExpansion[];e:Event[];}{
+  onStart(_data:GameData,_before:GameData,_event:StartEvent,_sender:Player,_updater:RequestUpdater):{r:Request[];e:Event[];}{
     const request=new FileRequest(
       "both",
       "coexistence",
@@ -13,22 +13,17 @@ export default class Ennui extends ModBase{
       fs.readFileSync("src/mods/shogi2-ennui/src/assets/effect1.png").toString("base64"),
       "image/png"
     );
-    return {r:[{request}],e:[]};
+    return {r:[request],e:[]};
   }
-  onMove(_d: GameData, before: GameData, event: MoveEvent, _sender: Player, _updater: RequestUpdater): { r: RequestExpansion[]; e: Event[]; } {
+  onMove(_d: GameData, before: GameData, event: MoveEvent, _sender: Player, _updater: RequestUpdater): { r: Request[]; e: Event[]; } {
     const data={...before};
     const {x,y}=event.to;
     const index=data.board.squares.findIndex((square)=>{
       return square.position.x===x && square.position.y===y;
     });
-    const diff:Diff={
-      keys:["board","squares",index,"image"],
-      type:"update",
-      value:"effect1"
-    };
     const square=data.board.squares[index];
     square.image="effect1";
     const request=new SquareRequest("both","obedience",square);
-    return {r:[{request,diff}],e:[]};
+    return {r:[request],e:[]};
   }
 };
